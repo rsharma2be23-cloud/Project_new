87f2211b21d11bf34d18f000008605239c807fe2
Rayan <rsharma2_be23@thapar.edu>
2026-06-23 22:38:29 +0530

    added update feature

--- src/app.js
+++ src/app.js
@@ -20,9 +20,30 @@ app.get('/notes',(req,res)=>{
 
   res.status(200).json({
     message:"notes fetched succesfully",
-    Notes:notes
+    Notes:notes// gets the notes array
   })
 })
+
+app.delete('/notes/:index',(req,res)=>{
+  const Index=req.params.index
+  delete notes[Index]// perfroms deletion
+  res.status(200).json({
+    message:"note has been deleted"
+  })
+})
+
+app.patch('/notes/:index',(req,res)=>{
+  const Index=req.params.index
+  const description=req.body.description // new desc
+  const title=req.body.title// new title
+  notes[Index].description=description//replace old desc with new updated desc 
+  notes[Index].title=title
+  res.status(200).json({
+    message: "note updated succesfully"
+  })
+
+})
+
 
 module.exports=app
 
