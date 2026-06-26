04cefdbbaf50c41bb7f8ff5719b35c4e6084f9eb
Rayan <rsharma2_be23@thapar.edu>
2026-06-27 01:01:49 +0530

    debugs

--- src/app.js
+++ src/app.js
@@ -18,7 +18,43 @@ app.post("/notes",async (req,res)=>{
     message:"Note Created woo!!"
   })
 })
+
+
+app.get("/notes",async(req,res)=>{
+
+  const notes= await noteModel.find()
+
+  res.status(200).json({
+    message:"we got da nodes!!",
+    notes:notes
+  })
+
+})
+
+app.delete("/notes/:id",async(req,res)=>{
+  const id=req.params.id
+  await noteModel.findOneAndDelete({
+    _id:id// _id because thats how it is stored in mongodb
+  })
+  res.status(200).json({
+    message:"Node is gonee!"
+  })
+})
+
+app.patch("/notes/:id",async(req,res)=>{
+  const id=req.params.id
+  const description=req.body.description
+
+  await noteModel.findOneAndUpdate({_id:id},{description:description})
+
+  res.status(200).json({
+    message:"Node has been updated Y'all "
+  })
+  
+})
+
 module.exports=app
+
 
 
 
