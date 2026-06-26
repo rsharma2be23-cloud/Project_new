cc40e7de62895c33e7d5fcdd48cc68a552b7549d
Rayan <rsharma2_be23@thapar.edu>
2026-06-26 23:58:58 +0530

    crud operations

--- src/app.js
+++ src/app.js
@@ -22,24 +22,3 @@ module.exports=app
 
 
 
-const express=require('express');
-const noteModel=require("./models/note.model")
-
-const app=express();
-
-app.use(express.json());// Middleware
-
-
-// CRUD apis
-
-app.post("/notes",async (req,res)=>{
-  const data=req.body
-  await noteModel.create({
-    title: data.title,
-    description: data.description
-  })
-  res.status(201).json({
-    message:"Note Created woo!!"
-  })
-})
-module.exports=app
