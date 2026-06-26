3839af1e9b49f4380ac32a2d761a0b90c5be269b
Rayan <rsharma2_be23@thapar.edu>
2026-06-26 23:57:26 +0530

    server and DB connection

--- src/app.js
+++ src/app.js
@@ -1,5 +1,21 @@
 const express=require('express');
+const noteModel=require("./models/note.model")
 
 const app=express();
 
+app.use(express.json());// Middleware
+
+
+// CRUD apis
+
+app.post("/notes",async (req,res)=>{
+  const data=req.body
+  await noteModel.create({
+    title: data.title,
+    description: data.description
+  })
+  res.status(201).json({
+    message:"Note Created woo!!"
+  })
+})
 module.exports=app
--- src/db/db.js
+++ src/db/db.js
@@ -3,8 +3,10 @@ const mongoose=require('mongoose');
 
 async function connectDB() {
 
-
-  await mongoose.connect("mongodb+srv://RayanSharma:EagxXguxD3tLz7-@cluster0.hrdlmqk.mongodb.net/Pluto")
+// js wont move to next line until this await func is finished
+  await mongoose.connect("mongodb+srv://RayanSharma:EagxXguxD3tLz7-@cluster0.hrdlmqk.mongodb.net/Pluto")// pluto is the DB name this function will automatically create pluto DB
 
   console.log("connected to DB")
 }
+
+module.exports=connectDB
