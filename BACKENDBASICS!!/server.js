04e91ae1ff422c70f21b297db4f22f3c718c1eca
Rayan <rsharma2_be23@thapar.edu>
2026-06-28 23:03:12 +0530

    server issues fixed

--- BACKENDBASICS!!/server.js
+++ BACKENDBASICS!!/server.js
@@ -7,11 +7,3 @@ app.listen(3000,()=>{
   console.log("Server Started on PORT 3000!!")
 })
 
-// start the server
-
-const app=require("./src/app")
-
-
-app.listen(3000,()=>{
-  console.log("Server Started on PORT 3000!!")
-})
